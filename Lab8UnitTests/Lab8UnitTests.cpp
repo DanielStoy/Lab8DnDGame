907826df9795b84050c45c4bac910bad39c81dea
Stoy, Daniel (stoyda) <stoyda@mail.uc.edu>
2019-10-28 12:39:25 -0400

    Add project files.

---
+++ Lab8UnitTests/Lab8UnitTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab8UnitTests
+{
+	TEST_CLASS(Lab8UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
