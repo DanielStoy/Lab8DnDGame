b9db0e2545e39ae6cfffd861cf2aa1221f38cb86
Stoy, Daniel (stoyda) <stoyda@mail.uc.edu>
2019-10-28 12:44:03 -0400

    Removing worthless comments

--- Lab8DnDGameDll/Lab8DnDGameDll.cpp
+++ Lab8DnDGameDll/Lab8DnDGameDll.cpp
@@ -1,20 +1,11 @@
-// Lab8DnDGameDll.cpp : Defines the exported functions for the DLL.
-//
-
 #include "framework.h"
 #include "Lab8DnDGameDll.h"
 
-
-// This is an example of an exported variable
-LAB8DNDGAMEDLL_API int nLab8DnDGameDll=0;
-
-// This is an example of an exported function.
 LAB8DNDGAMEDLL_API int fnLab8DnDGameDll(void)
 {
     return 0;
 }
 
-// This is the constructor of a class that has been exported.
 CLab8DnDGameDll::CLab8DnDGameDll()
 {
     return;
